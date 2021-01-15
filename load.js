cf127c9d192b38789e98e6a4dc0859cb6b6cbec1
i-ices <3268131637@qq.com>
2021-01-15 11:22:55 +0800

    add load js

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
-let chaoXing = function () {
+;!(function () {
     const timeout = prompt("设置刷新时间间隔[S]");
     // 获取当前的URL
     const current = location.href;
@@ -20,4 +20,4 @@ let chaoXing = function () {
         // 时间间隔不大于0，仅刷新一次
         location.replace(current);
     }
-}
+})();
